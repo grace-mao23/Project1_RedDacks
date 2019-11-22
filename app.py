5ae7c55b207b79b4f922f32b7cf321e7825746cd
nkhan01 <nkhan01@stuy.edu>
2019-11-21 21:24:20 -0500

    updated devlog, finished login & singup, edited app.py as necessary

--- app.py
+++ app.py
@@ -3,13 +3,42 @@
 # P01
 # 11/13/2019
 
-from flask import Flask, render_template, request, redirect, url_for
-import urllib.request, json
+from flask import Flask, render_template, request, redirect, url_for, session, flash
+
+import urllib.request, json, sqlite3, os
+
 app = Flask(__name__)
 
+
+
+def checkAuth():
+    if "userID" in session:
+        return True
+    else:
+        return False
+
 @app.route("/")
-def helloworld():
-    return "bing bong"
+def root():
+    if checkAuth(): #if you've already logged in
+        return redirect(url_for('homeb'))
+    else: #if not, redirect to login page
+        return redirect(url_for('login'))
+
+@app.route("/login") #login page
+def login():
+    if checkAuth():
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html')
+
+@app.route("/signup") #signup page
+def signup():
+    if checkAuth():
+        return redirect(url_for('home'))
+    return render_template('signup.html')
+
+
+
 
 if __name__ == "__main__":
     app.debug = True
